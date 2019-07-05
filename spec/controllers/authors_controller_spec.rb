0a4193f81bb2630fbf31d55cc3fd17ce410dac09
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-07-05 12:32:21 +0000

    Automatically backed up by Learn

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
-type: :request
+
 RSpec.describe AuthorsController do
   let(:jeff) { Author.create!(name: "H. Jeff", email: "jeff@sbahj.info") }
   let(:jeff_found) { Author.find_by(email: jeff.email) }
@@ -46,5 +46,4 @@ RSpec.describe AuthorsController do
       expect(response).to render_template(:new)
     end
   end
-end
 end
